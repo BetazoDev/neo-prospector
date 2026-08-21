b72a733ad96efacbe582e8ac52079d7938111239
Humberto Alonso <diabolicalprojects@gmail.com>
2026-08-20 21:06:47 -0600

    fix(db): use standard PrismaClient with env URL instead of driver adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,13 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
-import { getDatabaseUrl } from './database'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 function createPrismaClient() {
-  const connectionString = getDatabaseUrl()
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+globalForPrisma.prisma = prisma
